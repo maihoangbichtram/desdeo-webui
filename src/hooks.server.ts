e40ca7e9f7b713636579f1df92a3fb60709fbcb8
Hoang Bich Tram Mai <maihbt@jyu-mac-20240520.local>
2024-08-19 18:38:13 +0300

    Test hooks server

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -169,3 +169,21 @@ import type { Socket as SocketClient } from "socket.io-client";
         console.log(`socket ${id} has joined room ${room}`);
       });
   });*/
+
+import http from "http";
+import { handler } from '../build/handler.js'; // <- Import SvelteKit handlers
+import injectSocketIO from "../socketIoHandler.js"; // The SocketIO stuff (see next step)
+import express from 'express';
+
+const app = express();
+const server = http.createServer(app);
+
+// Inject SocketIO
+injectSocketIO(server)
+
+// SvelteKit handlers
+app.use(handler);
+
+server.listen(3000, () => {
+    console.log('Running on http://localhost:3000');
+});
